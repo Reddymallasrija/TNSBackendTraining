b361c2ed218cd08f3bd1a315f796b0bd0e5db9ff
Reddymallasrija <reddymallasreeja@gmail.com>
2025-11-04 22:04:01 +0530

    Add files via upload

---
+++ MainEnhancedfor.java
@@ -0,0 +1,8 @@
+public class MainEnhancedfor {
+    public static void main(String[] args){
+        String[] arr={"srija","jhansi","priya","padma","sidhhU"};
+        for (String a:arr){
+            System.out.println(a);
+        }
+    }
+}
